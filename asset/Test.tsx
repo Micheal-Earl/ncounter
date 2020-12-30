c24554fe70c6c29a572f8c22459ed965ebf83352
Micheal Earl <michealearldev@gmail.com>
2020-12-29 18:55:37 -0800

    init

---
+++ asset/Test.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Test" tilewidth="32" tileheight="32" tilecount="224" columns="14" objectalignment="topleft">
+ <grid orientation="isometric" width="32" height="32"/>
+ <image source="asset/IsometricTiles.png" width="448" height="537"/>
+</tileset>
---
+++ asset/Testo.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Testo" tilewidth="60" tileheight="64" tilecount="1" columns="0">
+ <grid orientation="isometric" width="1" height="1"/>
+ <tile id="27">
+  <image width="60" height="64" source="asset/blocks/block (1).png"/>
+ </tile>
+</tileset>
