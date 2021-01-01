c9412e084d07eb1a71c64b47cf177dfbb612d293
Micheal Earl <michealearldev@gmail.com>
2021-01-01 14:33:06 -0800

    Basic changes, changed asset folder name

--- src/main.js
+++ src/main.js
@@ -1,30 +1,46 @@
-function main() {
-    console.log("neat");
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+// Preload scene function. Phaser finds and executes this function when it starts.
+function preload() {
+    console.log(this); // Idk what 'this' is in this context
+
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude', 
+        'assets/dude.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
 }
 
-main();
+function create() {
+    this.add.image(400, 300, 'sky');
+    this.add.image(400, 300, 'star');
 
+    let platforms = this.physics.add.staticGroup();
 
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+}
 
-/*
-// Load HTTP module
-const http = require("http");
+function update() {
 
-const hostname = "127.0.0.1";
-const port = 8000;
-
-// Create HTTP server
-const server = http.createServer((req, res) => {
-
-   // Set the response HTTP header with HTTP status and Content type
-   res.writeHead(200, {'Content-Type': 'text/plain'});
-
-   // Send the response body "Hello World"
-   res.end('Hello World\n');
-});
-
-// Prints a log once the server starts listening
-server.listen(port, hostname, () => {
-   console.log(`Server running at http://${hostname}:${port}/`);
-})
-*/
+}
