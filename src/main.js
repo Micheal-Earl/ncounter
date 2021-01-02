ed153170c83ce953e97b177549f461b68a73388c
Micheal Earl <michealearldev@gmail.com>
2021-01-01 17:23:00 -0800

    Made phaser intellisense work

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-var config = {
+let config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
@@ -16,7 +16,9 @@ var config = {
     }
 };
 
-var game = new Phaser.Game(config);
+
+let game = new Phaser.Game(config);
+let player;
 
 // Preload scene function. Phaser finds and executes this function when it starts.
 function preload() {
@@ -32,15 +34,60 @@ function preload() {
     );
 }
 
+
 function create() {
+    // Background
     this.add.image(400, 300, 'sky');
-    this.add.image(400, 300, 'star');
 
+    // Platforms
     let platforms = this.physics.add.staticGroup();
-
     platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    platforms.create(500, 400, 'ground');
+
+    // Player
+    player = this.physics.add.sprite(100, 450, 'dude');
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'dude', frame: 4 } ],
+        frameRate: 20
+    });
+    
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    // Colliders
+    this.physics.add.collider(player, platforms);
 }
 
 function update() {
+    let cursors = this.input.keyboard.createCursorKeys();
 
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+        player.anims.play('left', true);
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+        player.anims.play('right', true);
+    } else {
+        player.setVelocityX(0);
+        player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-330);
+    }
 }
